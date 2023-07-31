e0b3960244816f5fcf16ace678caa8f448d98611
Влад Веселов <veselo.vlad9976@gmail.com>
2023-07-31 22:55:00 +0300

    Change code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price)
+    public int calculate(int price) {
 
-    {
-
-        int result = price /20;
+        int result = price / 20;
         return result;
     }
 }
